c03fa25ae87cfab71aa49313658666841c37f553
Patrick Thibodeau <patrick.thibodeau@gmail.com>
2019-09-18 13:34:36 -0500

    fixed global scope issue problem 13

--- practice.js
+++ practice.js
@@ -148,7 +148,7 @@ function pond() {
 //as strings.
 
 //This array should contain the variable names (as strings) accessible in the global scope.
-let globalScope = ["duck", "rubberDuck", "realDuck", "realDuck"];
+let globalScope = ["duck"];
 
 //This array should contain the variable names (as strings) accessible in the bathroom function.
 let bathroomScope = ["duck", "rubberDuck"];
